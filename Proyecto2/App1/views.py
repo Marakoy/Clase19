9c3c01fd1bb523e1e2b3481abf0eb2327f41a6ea
marakoy <marcosrafaelblanco@gmail.com>
2023-04-17 22:00:15 -0300

    Clase 19

--- Proyecto2/App1/views.py
+++ Proyecto2/App1/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 from App1.models import Curso
 from django.http import HttpResponse
 
-def curso(self):
-    curso = Curso(nombre="vista",curso=19673)
-    curso.save()
-    texto= f"----> Asignatura: {curso.nombre}, curso: {curso.curso}"
-    return HttpResponse(texto)
+# Create your views here.
+def inicio(request):
+    return render(request,'App1/inicio.html')
+def cursos(request):
+    return render(request,'App1/cursos.html')
+def profesores(request):
+    return render(request,'App1/profesores.html')
+def estudiantes(request):
+    return render(request,'App1/estudiantes.html')
+def entregables(request):
+    return render(request,'App1/entregables.html')
 
 # Create your views here.
